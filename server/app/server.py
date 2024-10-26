7a203425659f5c63fa046de351800aa1346afeca
George Stroescu <george.ionatan.stroescu@gmail.com>
2024-10-26 14:28:25 +0300

    added endpoint to receive data from extension

--- server/app/server.py
+++ server/app/server.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+import json
+import uvicorn
 
 load_dotenv()
 
@@ -11,6 +13,13 @@ app = FastAPI()
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
+@app.put("/send-ext-data")
+async def send_data(jsonstring):
+    data = json.load(jsonstring)
+
+    # for k, v in data.items():
+    #     print(k, v)
+    return {"message": data}
+
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
