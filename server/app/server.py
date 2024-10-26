40cdc8ad98ff5d0bd7733c356d9a9b19bce2f42a
George Stroescu <george.ionatan.stroescu@gmail.com>
2024-10-26 14:37:49 +0300

    Update server.py

--- server/app/server.py
+++ server/app/server.py
@@ -1,14 +1,13 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from langchain_openai import ChatOpenAI
 import json
 import uvicorn
 
 load_dotenv()
 
 app = FastAPI()
-# llm = ChatOpenAI(model="gpt-4o-mini")
-
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
@@ -20,6 +19,9 @@ async def send_data(jsonstring):
     # for k, v in data.items():
     #     print(k, v)
     return {"message": data}
+
+def send_data_to_openai(data):
+    llm = ChatOpenAI(model="gpt-4o-mini")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
